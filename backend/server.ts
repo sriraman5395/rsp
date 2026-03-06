a87364484ee3799007119ef051f11fdc7c4b71ae
sriraman5395 <sriraman5395@gmail.com>
2026-03-06 16:49:08 +0530

    push

--- backend/server.ts
+++ backend/server.ts
@@ -11,7 +11,7 @@ import path from "path";
 
 async function startServer() {
   const app = express();
-const PORT = 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
   app.use(cors());
   app.use(express.json());
@@ -19,6 +19,13 @@ app.use("/uploads", express.static("uploads"));
 
   // API Routes
   app.use("/api", apiRouter);
+
+  const frontendPath = path.join(__dirname,'../frontend/dist');
+  app.use(express.static(frontendPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
 
   // Vite middleware for development
 
