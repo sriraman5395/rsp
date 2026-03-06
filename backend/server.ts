6f6b4991ec92a48896b69d95dbf322346bc98ffb
sriraman5395 <sriraman5395@gmail.com>
2026-03-06 17:00:49 +0530

    file changed

--- backend/server.ts
+++ backend/server.ts
@@ -23,7 +23,7 @@ app.use("/uploads", express.static("uploads"));
   const frontendPath = path.join(__dirname,'../frontend/dist');
   app.use(express.static(frontendPath));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 
